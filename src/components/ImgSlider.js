2e812e5db3a6c488600d4a8be036c82fda658986
Farhan <farhanmaqsood10@hotmail.com>
2021-08-27 01:21:40 +0500

    ADD: Viewers

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -12,7 +12,7 @@ function ImgSlider() {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-    }
+    };
     return (
         <Carousel{...settings}>
             <Wrap>
@@ -29,12 +29,12 @@ export default ImgSlider
 
 
 const Carousel = styled(Slider)`
-    margin-top 20px;
+    margin-top: 20px;
 
     ul li button {
         &:before {
             font-size: 10px;
-            color: rbg(150,158,171);
+            color: rgb(150, 158, 171);
         }
     }
     
@@ -42,25 +42,32 @@ const Carousel = styled(Slider)`
         color: white;
     }
 
-    .slick-list{
+    .slick-list {
         overflow: visible;
     }
 
+    button {
+        z-index: 1;
     }
+
 `
 
 const Wrap = styled.div`
+    
     cursor: pointer;
+    
     img {
         border: 4px solid transparent;
         border-radius: 8px;
         width: 100%;
         height: 100%;
-        box-shadow: rbg(0 0 0 / 69%) 0px 26px 30px -10px,
-        rbg(0 0 0 / 73%) 0px 16px 10px -10px;
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
         transition-duration: 300ms;
-    }
+     
     &:hover {
-        border: 4px solid rbg(249, 249, 249, 0.8);
-    }
+        border: 4px solid rgb(249, 249, 249, 0.8);
+       }
+   }
+
 `
